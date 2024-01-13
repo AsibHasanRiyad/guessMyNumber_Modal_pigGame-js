9fb22b42d9893105f6d5f32efe2261a85533b442
Sayad Riyad <sayadriyad@Sayads-MacBook-Air.local>
2024-01-13 23:54:40 +0600

    new

---
+++ modal.js
@@ -0,0 +1,30 @@
+"use strict";
+
+const modal = document.querySelector(".modal");
+const closeModal = document.querySelector(".close-modal");
+const overlay = document.querySelector(".overlay");
+const showModal = document.querySelectorAll(".show-modal");
+const openModal = () => {
+  modal.classList.remove("hidden");
+  overlay.classList.remove("hidden");
+};
+const close = () => {
+  modal.classList.add("hidden");
+  overlay.classList.add("hidden");
+};
+
+for (let i = 0; i < showModal.length; i++) {
+  showModal[i].addEventListener("click", openModal);
+}
+
+// closeModal.addEventListener("click", () => {
+//   modal.classList.add("hidden");
+//   overlay.classList.add("hidden");
+// });
+
+// overlay.addEventListener("click", () => {
+//   modal.classList.add("hidden");
+//   overlay.classList.add("hidden");
+// });
+closeModal.addEventListener("click", close);
+overlay.addEventListener("click", close);
